e2f100b9b2b55bcb50ef5f7cb4abfe58e88ec698
Nadiasharmin017 <nadiasharmin017@gmail.com>
2025-07-10 00:03:04 +0600

    hidden the header section

---
+++ js/buyticket.js
@@ -0,0 +1,5 @@
+//when click buy ticket then hidden the header section
+function buyTicketBtn(){
+    const buyTicketButton = document.getElementById("header-section");
+    buyTicketButton.classList.add("hidden");
+}
