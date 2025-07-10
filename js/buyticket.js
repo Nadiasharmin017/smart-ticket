191e6d8e29943f961c7d1a99e689840cc0c50fd8
Nadiasharmin017 <nadiasharmin017@gmail.com>
2025-07-10 10:19:25 +0600

    add and remove hidden part

--- js/buyticket.js
+++ js/buyticket.js
@@ -2,4 +2,11 @@
 function buyTicketBtn(){
     const buyTicketButton = document.getElementById("header-section");
     buyTicketButton.classList.add("hidden");
+
+    // this is only show header section[home section], hidden the body part 
+    const homeButton = document.getElementById("body-part");
+    homeButton.classList.remove("hidden");
 }
+
+
+
